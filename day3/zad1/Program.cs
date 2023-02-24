edb7dcce5233b85f938dfc730bb452771dda1f82
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-02-24 16:26:07 +0300

    3день

---
+++ day3/zad1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите число а: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            A arel = new A(num1, num2);
+            arel.vzv();
+            arel.vkz();
+            Console.ReadLine(); 
+        }
+        class A
+        {
+            public double a = 1; // поле данных
+            public double b = 2; // поле данных
+
+            public A(double a, double b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+
+            public void vzv()
+            {
+                Console.WriteLine($"{Math.Pow((a + b), 2)}");
+            }
+
+            public void vkz()
+            {
+                Console.WriteLine($"{(Math.Sin(b) + 4) / (2 * a)}");
+            }
+            
+        }
+
+    }
+}
---
+++ day3/zad2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product[] products = new Product[]
+            {
+                new MilkTovar("Cheese",5.15M,14),
+                new Igrushka("Mishka",20M,"Sweet"),
+                new Tovar("Soufa", 10000M)
+            };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+            //Tovar arel = new Igrushka("Pony");
+            //arel.Naz();
+            Console.ReadLine();
+        }
+
+
+        class Igrushka:Tovar
+        {
+            public string Kind { get; set; }
+            public Igrushka(string name,decimal cost, string kind):base(name,cost)
+            {
+               
+                Kind = kind;
+            }
+
+            public override string ToString()
+            {
+                return $"Kind - {Kind} "; 
+            }
+            //public void naz()
+            //{
+            //    console.writeline($"игрушка - {name}");
+            //
+
+
+        }
+        class Product 
+        {
+            public string Name { get; set; }
+            
+            //public int Cost { get; set; }
+            //public int day = 14;
+            //public string Nazvanie_e = "Tv";
+            //public int Huge = 15;
+
+
+            public Product(string name)
+            {
+                Name = name;
+                //Cost = cost;
+            }
+
+            public override string ToString()
+            {
+                return $"Name - {Name} ";
+            }
+            //public void AboutProduct()
+            //{
+            //    Console.WriteLine($"Продукт: {Name}");
+            //}
+        }
+        class Tovar:Product
+        {
+            public decimal Cost { get; set; }
+            public Tovar(string name, decimal cost):base(name)
+            {
+                Cost = cost;
+            }
+
+            public override string ToString()
+            {
+                return base.ToString()+
+                    $"Cost - {Cost} ";
+            }
+            //public void AboutTovar()
+            //{
+            //    base.AboutProduct();
+            //    Console.WriteLine($"Cost - {Cost} ");
+            //}
+        }
+        class MilkTovar : Tovar
+        {
+            public int Days { get; set; }
+
+            public MilkTovar(string name, decimal cost, int days):base(name,cost)
+            {
+
+                Days = days;
+            }
+            public override string ToString()
+            {
+                return base.ToString()+
+                    $"Days - {Days} "; 
+            }
+            //public void AboutMilkTovar()
+            //{
+            //    Console.WriteLine($"Название молочного товара: {Nazvanie}, Срок годности в течении {day} дней ");
+            //}
+        } 
+    }
+}
