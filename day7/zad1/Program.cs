859d5ad7261c227055d22cb90074c138d1baa803
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-03-01 14:16:56 +0300

    day7

---
+++ day7/zad1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            var str = Console.ReadLine().ToLower();
+            Console.WriteLine($"Жы в тексте: {str.Contains("жы")}");
+            Console.WriteLine($"Шы в тексте: {str.Contains("шы")}");
+            Console.WriteLine($"Чя в тексте: {str.Contains("чя")}");
+            Console.WriteLine($"Щя в тексте: {str.Contains("щя")}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ day7/zad2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace zad2
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            if (!VvodVerniy(s))
+            {
+                Console.WriteLine("Необходимо ввести только цифры и буквы");
+                return;
+            }
+            int sum = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    sum += Convert.ToInt32(s[i].ToString());
+                }
+            }
+            if (sum == s.Length)
+            {
+                Console.WriteLine("Сумма цифр в строке = длине строки");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Сумма цифр в строке != длине строки");
+                Console.ReadLine();
+            }
+        }
+        static bool VvodVerniy(string inputStr)
+        {
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(inputStr[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+            Console.ReadLine();
+        }
+        
+    }
+}
