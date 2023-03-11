3baf53456b6d23171266b381740bacb463686970
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-03-11 09:46:43 +0300

    day15

---
+++ day15/zad2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace zad2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(PotokOperciy));
+
+            thread1.Start();
+            thread1.Join();
+
+        }
+        static void PotokOperciy() { 
+        Stopwatch sWatch = new Stopwatch();
+            sWatch.Start();
+            int step = 0;
+            Console.WriteLine("Potok is Run!!!!!!");
+            for (int i = 0; i <= 10; i++)
+            {
+                step += i;
+            }
+            Console.WriteLine(step);
+            sWatch.Stop();
+            Console.WriteLine(sWatch .ElapsedMilliseconds.ToString());
+        }
+    }
+}
---
+++ day15/zad3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(First));
+            Thread thread2 = new Thread(new ThreadStart(Second));
+
+            thread1.Start();
+            thread1.Join();
+            thread2.Start();
+            thread2.Join();
+
+         
+        }
+        static void First() 
+        {
+            Console.WriteLine("Введите А: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Первый поток");
+            int item = 0;
+            
+            for (int i = A; i < N; i++)
+            {
+                item +=  Convert.ToInt32(Math.Pow(A, N));
+            }
+            Console.WriteLine(item);
+
+        }
+        static void Second()
+        {
+            Console.WriteLine("Введите А: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второй поток");
+            int item = 1;
+            
+            for (int i = A; i < N; i++)
+            {
+                item += item * Convert.ToInt32(Math.Pow(A, N));
+            }
+            Console.WriteLine(item);
+            
+
+        }
+    }
+}
