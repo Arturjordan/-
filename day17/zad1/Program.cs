0595bd0554d70a500cdcec9e553e2eb6f70228f0
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-03-15 08:59:32 +0300

    day17

---
+++ day17/zad1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[25];
+            int countNeqative = 0;
+            Random random = new Random();
+            using (StreamWriter _stwriter = new StreamWriter(@"C:\input.txt"))
+                for (int i = 0; i < mass.Length; i++)
+                    _stwriter.WriteLine(random.Next(-100, 100));
+            using (StreamReader _stread = new StreamReader(@"C:\input.txt"))
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = int.Parse(_stread.ReadLine());
+                }
+            }
+            int max = mass.Max();
+            int min = mass.Min();
+            for (int e = 0; e < mass.Length; e++)
+            {
+                if (mass[e] <= 0)
+                {
+                    countNeqative++;
+                }
+            }
+            Console.WriteLine("Максимум:{0}\nМинимум:{1}\nКоличество отрицательных цифр:{2}", max, min, countNeqative);
+
+        }
+    }
+}
---
+++ day17/zad4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text;
+            using (StreamReader sr = new StreamReader("C:\\Users\\Admin\\Desktop\\1input.txt"))
+            {
+                text = sr.ReadToEnd().ToCharArray();
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '0')
+                    text[i] = '1';
+                else
+                {
+                    if (text[i] == '1')
+                        text[i] = '0';
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\Admin\\Desktop\\2input.txt"))
+            {
+                foreach (char c in text)
+                    sw.Write(c);
+            }
+        }
+    }
+}
