7128cbe25292d9d3ee353adc091ffe4e26ac3325
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-03-21 10:05:07 +0300

    day23

---
+++ day23/zad2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox2.Text);
+            double y = Convert.ToDouble(textBox1.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+            // Ввод исходных данных в окно результатов
+            textBox4.Text = "Результаты работы программы " +
+            "ст. Петрова И.И. " +
+            Environment.NewLine;
+            textBox4.Text += "При X = " + textBox1.Text +
+            Environment.NewLine;
+            textBox4.Text += "При Y = " + textBox2.Text +
+            Environment.NewLine;
+            textBox4.Text += "При Z = " + textBox3.Text +
+            Environment.NewLine;
+
+            // Вычисление выражения
+            double u;
+            if (x > Math.Abs(z))
+                u = 2 * Math.Pow(x, 3) + 3 * Math.Pow(z, 2);
+            else
+            if (3 < x)
+                u = Math.Abs(Math.Pow(x,2) - z);
+            else
+                u = Math.Pow(Math.Abs(Math.Pow(x, 2) - z), 2);
+
+            // Вывод результата
+            textBox4.Text += "U = " + u.ToString() +
+            Environment.NewLine;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
