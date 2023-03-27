a3cbf59531774468eab4ba1e9575cae1faac30a7
Arturjordan <78102586+Arturjordan@users.noreply.github.com>
2023-03-27 19:27:14 +0300

    day26

---
+++ day26/z1_Teoria/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace z1_Teoria
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ day26/z3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace z3
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            chart.Series = new SeriesCollection { new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>(),
+            PointGeometrySize = 0,
+                    StrokeThickness = 4,
+                    Fill = Brushes.Transparent
+                }
+            };
+
+            DataContext = this;
+        }
+
+        private void buttonCaculate_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            try
+            {
+                int minX = Convert.ToInt32(textBoxMinX.Text);
+                int maxX = Convert.ToInt32(textBoxMaxX.Text);
+
+                if (minX < maxX)
+                {
+                    ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+                    for (int i = minX; i <= maxX; i++)
+                    {
+                        if (i != 0)
+                        {
+                            points.Add(new ObservablePoint(i, 1 / Math.Tan(i)));
+                        }
+                    }
+                    chart.Series = new SeriesCollection { new LineSeries
+                    {
+                        Values = points,
+                        PointGeometrySize = 0,
+                        StrokeThickness = 4,
+                        Fill = Brushes.Transparent
+                    } 
+                    };
+                }
+
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show(
+                    "Неправильные данные введенные в поля",
+                    "Неправильный формат",
+                    MessageBoxButton.OK);
+            }
+
+        }
+    }
+}
